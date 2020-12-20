cafc1630f524779ffd98e6c3e86dd4bcced9425b
qux-bbb <1147635419@qq.com>
2020-12-20 21:28:09 +0800

    init

---
+++ iptables_set.sh
@@ -0,0 +1,25 @@
+# clear iptables
+sudo iptables -t nat -F
+sudo iptables -F
+
+sudo iptables -t nat -A POSTROUTING -o wlp5s0 -s 192.168.56.0/24 -j MASQUERADE
+
+# Default drop.
+sudo iptables -P FORWARD DROP
+
+# Existing connections.
+sudo iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Accept connections from vboxnet to the whole internet.
+sudo iptables -A FORWARD -s 192.168.56.0/24 -j ACCEPT
+
+# Internal traffic.
+# sudo iptables -A FORWARD -s 192.168.56.0/24 -d 192.168.56.0/24 -j ACCEPT
+
+# Log stuff that reaches this point (could be noisy).
+sudo iptables -A FORWARD -j LOG
+
+
+# set ip forward
+echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward
+sudo sysctl -w net.ipv4.ip_forward=1
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    system("dir");
+    return 0;
+}
